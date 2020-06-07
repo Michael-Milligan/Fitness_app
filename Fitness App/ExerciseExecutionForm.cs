9c5b8447c56f055eaa5f688d74ccebfee81a3a3d
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-06-07 20:26:14 +0300

    Made Pause and began implementing changing of exercises

--- Fitness App/ExerciseExecutionForm.cs
+++ Fitness App/ExerciseExecutionForm.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Media;
 
 namespace Fitness_App
 {
@@ -15,6 +16,7 @@ namespace Fitness_App
         Label NameLabel;
         Exercise CurrentExercise;
         int ExerciseIndex;
+        SoundPlayer Player = new SoundPlayer("Whistle.mp3");
 
         public ExerciseExecutionForm(ExerciseComplex Complex)
         {
@@ -34,14 +36,13 @@ namespace Fitness_App
         public Grid BuildExercise(Exercise exercise)
         {
             Grid grid = new Grid();
-            grid.ShowGridLines = true;
             grid.ColumnDefinitions.Add(new ColumnDefinition());
             grid.ColumnDefinitions.Add(new ColumnDefinition());
 
             NameLabel = new Label();
             grid.RowDefinitions.Add(new RowDefinition());
+            NameLabel.HorizontalContentAlignment = HorizontalAlignment.Center;
             NameLabel.Content = "Name";
-            NameLabel.VerticalAlignment = VerticalAlignment.Top;
             grid.Children.Add(NameLabel);
             Grid.SetColumnSpan(NameLabel, 2);
             Grid.SetRow(NameLabel, 0);
@@ -49,12 +50,24 @@ namespace Fitness_App
             Quantity = new Label();
             grid.RowDefinitions.Add(new RowDefinition());
             Quantity.Content = exercise.Quantity;
-            Quantity.VerticalAlignment = VerticalAlignment.Bottom;
+            Quantity.HorizontalContentAlignment = HorizontalAlignment.Center;
             grid.Children.Add(Quantity);
             Grid.SetColumnSpan(Quantity, 2);
-            Grid.SetRow(Quantity, 0);
+            Grid.SetRow(Quantity, 1);
+
+            Button Previous = new Button();
+            grid.RowDefinitions.Add(new RowDefinition());
+            Previous.Content = "Previous";
+            grid.Children.Add(Previous);
+            Grid.SetColumn(Previous, 0);
+            Grid.SetRow(Previous, 2);
 
             Button Next = new Button();
+            grid.RowDefinitions.Add(new RowDefinition());
+            Next.Content = "Next";
+            grid.Children.Add(Next);
+            Grid.SetColumn( Next, 1);
+            Grid.SetRow(Next, 2);
 
             return grid;
         }
@@ -65,8 +78,50 @@ namespace Fitness_App
             {
                 Quantity.Content = (int)Quantity.Content - 1;
             }
-            if ((int)Quantity.Content == 0) 
-                ++ExerciseIndex;
+            if ((int)Quantity.Content == 0)
+            {
+                Player.Play();
+                Timer.Stop();
+                MakeAPause();
+            }
         }
+        public void MakeAPause()
+        {
+            StackPanel Panel = new StackPanel();
+
+            Label Message = new Label();
+            Message.HorizontalContentAlignment = HorizontalAlignment.Center;
+            Message.Content = "Rest! The remaining time:";
+            Panel.Children.Add(Message);
+
+            Label Time = new Label();
+            Time.Content = 15;
+            Time.HorizontalContentAlignment = HorizontalAlignment.Center;
+            Panel.Children.Add(Time);
+
+            Timer timer = new Timer(new TimerCallback(TimerOnTick), Time.Content, 0, 1000);
+            if ((int)Time.Content == 0)
+            {
+                Player.Play();
+                timer.Dispose();
+                NextExercise();
+            }
+            Application.Current.Windows[0].Content = Panel;
+        }
+
+        public void TimerOnTick(object Time)
+        {
+            Timer.IsEnabled = false;
+            Time = (int)Time - 1;
+            ((Application.Current.Windows[0].Content as StackPanel).Children[1] as Label).Content = Time;
+            Timer.Start();
+        }
+
+        public void NextExercise()
+        {
+            CurrentExercise = new Exercise(Complex.Exercises[++ExerciseIndex]);
+            Application.Current.Windows[0].Content = BuildExercise(CurrentExercise);
+        }
+                
     }
 }
